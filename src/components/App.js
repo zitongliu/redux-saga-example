1681de8180563454b7b2c1ae0516ee1adeff0048
zitongliu <zitongliu7@gmail.com>
2019-04-30 18:48:42 +1000

    added generator function example

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>Test</div>
-  );
+// function* testing() {
+//   yield 1;
+//   yield 2;
+//   yield 3;
+// }
+
+function* testing() {
+  // under the hood redux saga are all running while true. so the fourth console.log will be 1 again
+  while(true) {
+    yield 1;
+    yield 2;
+    yield 3;
+  }
+}
+
+class App extends React.Component {
+  render() {
+    const iterator = testing();
+    console.log(iterator.next());
+    console.log(iterator.next());
+    console.log(iterator.next());
+    console.log(iterator.next());
+    console.log(iterator.next());
+    console.log(iterator.next());
+
+    return (
+      <div>Test</div>
+    );
+  }
 }
 
 export default App;
